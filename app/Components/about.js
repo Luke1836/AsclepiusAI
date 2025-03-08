f3a9aa430bf81ab1ca28b1188e8327031a134c79
Luke1836 <codingluke2023@gmail.com>
2025-03-09 01:09:17 +0530

    About page styles

--- app/Components/about.js
+++ app/Components/about.js
@@ -48,8 +48,8 @@ export default function AboutPage()
   return (
     <section id="about" className="w-full h-[200vh] bg-[#2b3530]">
       <div className="w-full h-[100vh] flex flex-col lg:flex-row items-center gap-1">
-        <div className="text-[1rem] md:text-[2rem] text-[#CED1BF] capitalize w-[90%] lg:w-[35%] text-center p-4 grid place-content-center">
-          Welcome to AsclepiusAI – The Future of Predictive Healthcare
+        <div className="text-[1rem] md:text-[2rem] text-[#CED1BF] w-[90%] lg:w-[35%] text-center p-4 grid place-content-center">
+          Welcome to <span className='text-[#CA7D57]'>AsclepiusAI</span> – The Future of Predictive Healthcare
         </div>
         <div className="w-[90vw] lg:w-[55vw] h-[100vh] grid place-content-center">  
           <div className="section-1" ref={componentRef} >  
@@ -58,13 +58,27 @@ export default function AboutPage()
         </div>  
       </div>
 
-      <div className="w-full h-[100vh]">
-        <div className="w-[35%] h-[100vh] grid place-content-center">
+      <div className="w-full h-[100vh] flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-[35%] h-[5rem] md:h-[100vh]  grid place-content-center">
           <p className="flex flex-row gap-4 items-center">
-            <FontAwesomeIcon icon={ faBars } size="2xl" className="text-[#CED1BF] translate-y-1" />
+            <FontAwesomeIcon icon={ faBars } size="2xl" className="text-[#CED1BF] translate-y-1 hidden md:block" />
             <span className="text-[#CED1BF] capitalize text-[3rem]">Introduction</span>
           </p>
-          
+        </div>
+
+        <div className='md:w-[75%] w-full h-[90vh] md:h-[100vh] flex flex-col items-center justify-start md:justify-center gap-8'>
+          <p className='w-[80%] text-[#CED1BF] text-[1rem] lg:text-[1.5rem]'>
+            &nbsp;&nbsp;&nbsp;Step into a new era of medical intelligence with <span className='text-[#CA7D57]'>AsclepiusAI</span>, where cutting-edge artificial intelligence meets the precision of modern medicine. Designed to revolutionize long-term hypercholesterolemia prediction, <span className='text-[#CA7D57]'>AsclepiusAI</span> empowers healthcare professionals and individuals alike with unparalleled insights into cardiovascular health.
+          </p>
+          <p className='text-[#CED1BF] w-[80%] text-[1rem] lg:text-[1.5rem] hidden md:block'>
+            &nbsp;&nbsp;&nbsp;Harnessing the power of advanced machine learning, predictive analytics, and biomedical intelligence, AsclepiusAI meticulously evaluates risk factors, genetic predispositions, and lifestyle influences to provide personalized, proactive health forecasts.
+          </p>
+          <p className='text-[#CED1BF] w-[80%]'>
+            &nbsp;&nbsp;&nbsp;At AsclepiusAI, we don’t just predict—we prevent. Our mission is to equip individuals with the knowledge to make informed decisions, enabling a future where cardiovascular diseases are mitigated before they arise.
+          </p>
+          <p className='text-[#CED1BF] w-[80%]'>
+            🔬 Precision. Prevention. Progress. Welcome to the next frontier in AI-driven healthcare. 
+          </p>
         </div>
       </div>
     </section>
