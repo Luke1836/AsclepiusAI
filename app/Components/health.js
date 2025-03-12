38a6c8f68f845b1c70d38d246d767b8fccadef31
Luke1836 <codingluke2023@gmail.com>
2025-03-12 21:08:10 +0530

    Parallax effect 4

--- app/Components/health.js
+++ app/Components/health.js
@@ -3,54 +3,25 @@
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { useRef, useEffect } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
 
 export default function HealthPage()
 {
-  const componentRef = useRef(null);
-  const imageRef = useRef(null);
-
-  useEffect(() => {
-    if (componentRef.current && imageRef.current) {
-      gsap.fromTo(
-        imageRef.current,
-        { y: "-10%" },
-        {
-          y: "10%",
-          ease: "none",
-          scrollTrigger: {
-            trigger: componentRef.current,
-            start: "top bottom",
-            end: "bottom top",   
-            scrub: 1,               
-          },
-        }
-      );
-    }
-  }, []);
-
   return (
-    <section id="health" className="w-full h-svh bg-[#30493D] flex flex-col md:flex-row overflow-hidden gap-8 md:gap-0">
+    <section id="health" className="w-full h-svh bg-[#30493D] flex flex-col md:flex-row overflow-hidden gap-8 lg:gap-0">
       <div className="w-full md:w-[50%] h-[5rem] md:h-[100vh] grid place-content-center">
         <p className="flex flex-row gap-4 items-center md:hidden">
           <FontAwesomeIcon icon={ faBars } size="2xl" className="text-[#CED1BF] translate-y-1 hidden md:block" />
           <span className="text-[#CED1BF] capitalize text-[3rem]">Health</span>
         </p>
         <div className="hidden md:block w-full h-full pr-16 py-16" >
-          <div className="section-2" ref={componentRef} >  
-            <div className="parallax-image-2" />  
-          </div>  
-            {/* <img src='/Photo-mini-project.jpg' alt='health' loading='lazy' className='w-full h-full object-fill'/>  */}
+            <img src='/Photo-mini-project.jpg' alt='health' loading='lazy' className='w-full h-full object-fill'/> 
         </div>  
       </div>
 
       <div className='md:w-[60%] w-full h-[90vh] md:h-[100vh] flex flex-col items-center justify-start md:justify-center gap-8'>
           <p className='w-[80%] text-[#CED1BF] text-[1rem] xl:text-[1.5rem]'>
-            &nbsp;&nbsp;<span className='text-[#CA7D57]'>Hypercholesterolemia</span> commonly referred to as high cholesterol, is a condition in which excess cholesterol circulates in the bloodstream, increasing the risk of heart disease and stroke. Cholesterol is a fatty substance essential for cell function, but an imbalance—especially high levels of LDL (low-density lipoprotein) cholesterol—can lead to plaque buildup in the arteries, restricting blood flow.
+            &nbsp;&nbsp;&nbsp;<span className='text-[#CA7D57]'>Hypercholesterolemia</span> commonly referred to as high cholesterol, is a condition in which excess cholesterol circulates in the bloodstream, increasing the risk of heart disease and stroke. Cholesterol is a fatty substance essential for cell function, but an imbalance—especially high levels of LDL (low-density lipoprotein) cholesterol—can lead to plaque buildup in the arteries, restricting blood flow.
           </p>
           <p className='text-[#CED1BF] w-[80%] text-[1rem] xl:text-[1.5rem] hidden md:block'>
             &nbsp;&nbsp;&nbsp;Several factors contribute to <span className='text-[#CA7D57]'>Hypercholesterolemia</span>, some of which are controllable while others are genetic
