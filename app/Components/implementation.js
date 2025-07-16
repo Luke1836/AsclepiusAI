e4868f6dac6c186e1fa4fad314ee5b0e46c88e0d
Luke1836 <codingluke2023@gmail.com>
2025-07-16 18:43:36 +0530

    Styles changed

--- app/Components/implementation.js
+++ app/Components/implementation.js
@@ -32,16 +32,15 @@ export default function ImplementationPage() {
   }, []);
 
   return (
-    <section className="w-full bg-[#30493D] overflow-hidden">
+    <section className="w-full bg-gradient-to-tr to-[#00576d] from-[#110454] overflow-hidden">
       {/* Section 1 */}
       <div
         ref={(el) => {
           if (el) sectionRefs.current[0] = el;
         }}
-        className="w-full py-16 md:py-24 flex items-center justify-center bg-[#30493D] text-[#CED1BF] origin-bottom p-8"
-        style={{ transform: "skewY(6deg)" }}
+        className="w-full py-16 md:py-24 flex items-center justify-center bg-gradient-to-tr to-[#3c0134] from-fuchsia-900 text-[#CED1BF] origin-bottom p-8"
       >
-        <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative overflow-hidden">
+        <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative overflow-hidden" style={{ transform: "skewY(6deg)" }}>
           <img
             ref={(el) => {
               if (el) imageRefs.current[0] = el;
@@ -52,11 +51,11 @@ export default function ImplementationPage() {
           />
         </div>
 
-        <div className="w-full md:w-1/2 p-8 md:p-16 transform -skew-y-6">
+        <div className="w-full md:w-1/2 p-8 md:p-16 transform">
           <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center text-[#CA7D57] shadow-text">
             Data Preprocessing and Feature Engineering
           </h2>
-          <p className="text-lg">
+          <p className="text-lg text-justify">
             The dataset used for this study was obtained from a standard dataset and consists of multiple patient records containing clinical attributes (16) relevant to hypercholesterolemia prediction. The key steps in data preprocessing included: Handling Missing Values, Feature Selection, Normalization & Standardization, Categorical Encoding, and Train-Test Split.
           </p>
         </div>
@@ -68,9 +67,8 @@ export default function ImplementationPage() {
           if (el) sectionRefs.current[1] = el;
         }}
         className="w-full py-16 md:py-24 flex items-center justify-center flex-row-reverse text-[#CED1BF] origin-center p-8"
-        style={{ transform: "skewY(-6deg)" }}
       >
-        <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative overflow-hidden">
+        <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative overflow-hidden" style={{ transform: "skewY(-6deg)" }}>
           <img
             ref={(el) => {
               if (el) imageRefs.current[1] = el;
@@ -81,11 +79,11 @@ export default function ImplementationPage() {
           />
         </div>
 
-        <div className="w-full md:w-1/2 p-8 md:p-16 skew-y-6">
+        <div className="w-full md:w-1/2 p-8 md:p-16">
           <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center text-[#CA7D57] shadow-text">
             Machine Learning Models
           </h2>
-          <p className="text-lg">
+          <p className="text-lg text-justify">
             We experimented with multiple supervised learning models to determine the best algorithm for predicting long-term hypercholesterolemia risk. The models implemented were: Linear Regression, K-Nearest Neighbors, Decision Trees, Random Forest, Logistic Regression, and Support Vector Machine. Random Forest proved to be the superior model.
           </p>
         </div>
@@ -96,25 +94,25 @@ export default function ImplementationPage() {
         ref={(el) => {
           if (el) sectionRefs.current[2] = el;
         }}
-        className="w-full py-16 md:py-24 flex items-center justify-center bg-[#30493D] text-[#CED1BF] origin-bottom p-8"
-        style={{ transform: "skewY(6deg)" }}
+        className="w-full py-16 md:py-24 flex items-center justify-center bg-gradient-to-tr from-[#006b70] to-[#064b3f] text-[#CED1BF] origin-bottom p-8"
+        
       >
-        <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative overflow-hidden">
+        <div className="w-full md:w-1/2 h-[300px] md:h-[500px] relative overflow-hidden" style={{ transform: "skewY(6deg)" }}>
           <img
             ref={(el) => {
               if (el) imageRefs.current[2] = el;
             }}
-            src="/Futuristic.jpg"
+            src="/Computer.jpg"
             alt="Futuristic"
             className="absolute w-full h-full object-cover"
           />
         </div>
 
-        <div className="w-full md:w-1/2 p-8 md:p-16 -skew-y-6">
+        <div className="w-full md:w-1/2 p-8 md:p-16">
           <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center text-[#CA7D57] shadow-text">
             Framework and Tools
           </h2>
-          <p className="text-lg">
+          <p className="text-lg text-justify">
             The implementation used Python with Scikit-learn, Pandas, NumPy, Matplotlib, and Seaborn. Development environments included Jupyter Notebook and Google Colab.
           </p>
         </div>
